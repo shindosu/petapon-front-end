a71e7b5f8ccb821f6bb7b90932acc6d2b6fec504
Taisei Yamada <taiseiyamadashindosu@gmail.com>
2020-02-17 23:35:25 +0900

    test

--- src/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from "./components/app";
-
-const root = document.getElementById('root');
-if (root) {
-  ReactDOM.render(<App />, root);
-}
